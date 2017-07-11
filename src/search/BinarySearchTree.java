ecbbed6b7016b607f5fc82c1e5d8c5c9d0c8d025
Hanxiao Liu <t-hanxl@microsoft.com>
2017-07-11 17:15:46 +0800

    Codes for search

---
+++ src/search/BinarySearchTree.java
@@ -0,0 +1,31 @@
+package search;
+
+/**
+ * Created by t-hanxl on 7/11/2017.
+ */
+public class BinarySearchTree<T extends  Comparable<T>,V>{
+    private BSTNode root;
+
+    private static class BSTNode<T extends  Comparable<T>,V>{
+        T value;
+        private BSTNode left;
+        private BSTNode right;
+        private int N;
+
+        public BSTNode(T value,int N){
+            this.value=value;
+            this.N=N;
+        }
+
+        public int size(){
+            return 0;
+        }
+
+        public int size(BSTNode node){
+            return  0;
+        }
+
+        public
+
+    }
+}
